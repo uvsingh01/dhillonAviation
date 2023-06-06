481f085d9af2c823779b2dfd941f92da116b617a
uvsingh01 <mr.uvsingh@gmail.com>
2023-06-06 20:06:12 +0530

    homepage completed

--- components/Homepage/Hero.tsx
+++ components/Homepage/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
           className="w-full h-80 md:h-[60vh] slg:h-[75vh] xl:h-[100vh] "
           alt="VT_ZIN"
         />
-        <div  className="absolute top-0 inset-x-0 bottom-0 bg-black bg-opacity-20 font-bold flex items-end text-xl pb-12 ps-8 md:text-4xl md:pb-20 lg:pb-24 lg:text-5xl lg:ps-32 xl:text-7xl text-white">
+        <div  className="absolute top-0 inset-x-0 bottom-0 bg-black  bg-opacity-20 font-bold flex justify-center items-end text-xl pb-12  md:text-4xl md:pb-20 lg:pb-24 lg:text-5xl xl:text-7xl text-white">
         <div data-aos="fade-up">HELPING BUSINESS BECOME GREAT.</div>
         </div>
       </div>
